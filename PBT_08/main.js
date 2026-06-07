649efc8873e3381d91412594f826574212d056b5
Aris181106 <nvt181106@gmail.com>
2026-06-07 15:22:30 +0700

    [FEATURE] Add A3 assignment no8

--- PBT_08/main.js
+++ PBT_08/main.js
@@ -26,9 +26,6 @@ const TinhLuong = function(luong){
 console.log(TinhLuong(13));
 
 //Arrow Function
-const greet = (name) => {
-    return `Xin chào ${name}!`;
-};
 const totalWage = (wage) => {
     let wageAfterTaxes = 0;
     if(wage > 11){
@@ -40,3 +37,29 @@ const totalWage = (wage) => {
     }
 };
 console.log(totalWage(12));
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const EvenNums = nums.filter(n => n % 2 === 0);
+console.log ('Cac so chia het cho 2 la: ', EvenNums);
+
+const MultiplierNums = nums.map(n => n * 3);
+console.log ('Mang sau khi nhan voi 3 la: ', MultiplierNums);
+
+const SumNums = nums.reduce((total, n) => total + n, 0);
+console.log ('Tong cac phan tu trong mang la: ', SumNums);
+
+const FindNums = nums.find(n => n > 7)
+console.log ('Phan tu dau tien lon hon 7 la: ', FindNums);
+
+const FindNums2 = nums.every(n => n > 10)
+console.log ('Phan tu lon hon 10 la: ', FindNums2);
+
+const CheckNums = nums.every(n => n > 0)
+console.log ('Cac phan tu lon hon 0 la: ', CheckNums);
+
+const NumsDescription = nums.map(n => `Số ${n} là ${n % 2 === 0 ? 'chẵn' : 'lẻ'}`);
+console.log(NumsDescription);
+
+const ReversedNums = nums.reverse();
+console.log(ReversedNums)
